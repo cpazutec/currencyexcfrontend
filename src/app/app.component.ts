69b514937136a520ef4b0f12b0deff5ed17cc2f8
Christian Paz <cpaztrillo@gmail.com>
2024-05-12 21:41:04 -0500

    all

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
+import { Injectable } from '@angular/core';
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+const baseURL = 'http://127.0.0.1:5000/api/exchange';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
 export class AppComponent {
+  constructor(private httpClient: HttpClient) { }
   title = 'currencyexcfrontend';
+  amount!: number;
+  fromCurrency: string = 'USD';
+  toCurrency: string = 'SOL';
+  convertedAmount!: number;
+
+  convertCurrency() {
+    this.httpClient.get<Response>(`${baseURL}?from=${this.fromCurrency}&to=${this.toCurrency}&amount=${this.amount}`).subscribe(
+      response => {
+        this.convertedAmount = response.amount;
+      }
+    );
+  }
+}
+
+export interface Response {
+  amount: number,
+  currency: string
 }
